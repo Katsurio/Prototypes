f3786ee8bb5242111e1604f1b09fd1298c6f7749
John (Jef) Fisher <Fishe545@gmail.com>
2016-11-03 15:59:22 -0700

    super done

---
+++ php_includes/includes/data2.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Katsurio
+ * Date: 11/3/16
+ * Time: 12:35 PM
+ */
+$c = $c * 2;
+?>
---
+++ php_includes/main.php
@@ -0,0 +1,2 @@
+<p>Before eating McDonalds for a week straight.</p>
+<a href="http://watchmojo.com/uploads/blipthumbs/Fi-M-Top10-Another-Jackie-Chan-Moments-720p30_480.jpg"><img src="http://watchmojo.com/uploads/blipthumbs/Fi-M-Top10-Another-Jackie-Chan-Moments-720p30_480.jpg"></a>
