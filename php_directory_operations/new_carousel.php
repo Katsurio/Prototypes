24b5f4f750a75fbb45384814070a063a6214e2ec
John (Jef) Fisher <Fishe545@gmail.com>
2016-11-13 18:40:06 -0800

    Dan helped make the carousel not behave wonkily

--- php_directory_operations/new_carousel.php
+++ php_directory_operations/new_carousel.php
@@ -37,7 +37,10 @@
         transform: translate(-50%, -50%);
     }
     .prep{
-        transition-duration: 0s!important;
+        transition: none !important;
+    }
+    .slide{
+        transition-duration: .5s;
     }
     .mySlides {
         position: absolute;
@@ -53,13 +56,14 @@
         transform: translateX(100%);
         z-index: -4;
     }
-    .right-slide {
-        transform: translateX(100%);
-        z-index: -3;
+    .toCenter{
+        left: 0;
     }
-    .left-slide {
-        transform: translateX(-100%);
-        z-index: -2;
+    .toRight {
+        left:100%;
+    }
+    .toLeft {
+        left:-100%;
     }
 </style>
 <script>
@@ -68,34 +72,37 @@
     var index = 0;
 
     var current_image = 1;
-    showDivs(current_image);
+    //showDivs(current_image);
 
     function cycleImages(n) {
         console.log("plus divs function called");
-        showDivs(current_image += n);
+        showDivs(current_image + n);
     }
 
     function showDivs(n) {
         var i;
         var slides = $(".mySlides");
-        if (n > slides.length) {
-            current_image = 1
+        console.log('n will be '+n);
+        if(n<current_image){
+            prepClass= 'toRight';
+            hideClass = 'toLeft';
+        } else{
+            prepClass = 'toLeft';
+            hideClass = 'toRight';
         }
-        if (n < 1) {
-            current_image = slides.length
+        if (n >= slides.length-1) {
+            n = 0
         }
-        for (i = 0; i < slides.length; i++) {
-            $(slides[i]).addClass('hide-slide').removeClass('left-slide right-slide');
-//                .css('display','none');
-        }
-        $(slides[current_image-1]).addClass('show-slide').removeClass('hide-slide');
-        if(current_image == slides.length) {
-            $(slides[current_image-slides.length]).addClass('left-slide').removeClass('hide-slide');
-        } else {
-            $(slides[current_image]).addClass('left-slide').removeClass('hide-slide');
+        else if (n <= 0) {
+            n = slides.length-1
         }
 
+        $(slides[n]).removeClass('slide').addClass('prep')[0].offsetHeight;
+        $(slides[n]).removeClass('toLeft toRight').addClass(prepClass).removeClass('prep')[0].offsetHeight;;
+        $(slides[current_image]).removeClass('toCenter').addClass(hideClass+' slide');
+        $(slides[n]).removeClass('toRight toLeft prep').addClass('toCenter slide');
 
+        current_image = n;
         console.log("current image", current_image);
 //    .css('display', 'block')
     }
@@ -110,9 +117,12 @@
                 console.log(result);
                 $image_array = result.files;
                 for(var i = 0; i < $image_array.length; i++) {
-                    display_image(i);
+                    var $container = display_image(i);
+                    $container.addClass('toRight');
                 }
-                showDivs(current_image);
+                $container.removeClass('toRight');
+                current_image=i-1;
+                //showDivs(current_image);
 //                $('.carousel .mySlides:first').removeClass('hide-slide').addClass('show-slide');
             }
         });
@@ -120,8 +130,9 @@
     function display_image(index) {
         console.assert('display image called');
         $image = $('<img>').addClass('carousel-image').attr('src', $image_array[index]);
-        $img_album = $('<div>').addClass('mySlides hide-slide').append($image);
+        $img_album = $('<div>').addClass('mySlides ').append($image);
         $('.carousel-container').append($img_album);
+        return $img_album;
     }
 
     function applyClickHandlers() {
