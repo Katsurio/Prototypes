957a65b064f55fd0c1c604bfadf493ab6e1f8c66
John (Jef) Fisher <Fishe545@gmail.com>
2016-11-14 18:22:36 -0800

    saving before I leave LFZ

--- mysql_basics/mysql_queries.sql
+++ mysql_basics/mysql_queries.sql
@@ -1,1 +1,10 @@
 Place your queries below
+-- //FS1
+-- #1
+SELECT * FROM `users` WHERE `username` = 'Tandori'
+-- #2
+UPDATE `users` SET `email` ='TayTay@lfz.com' WHERE `username` = 'Tyrannosaurus'
+-- #3
+INSERT INTO `users` SET `username`='Pidgeon', `email`='Sean@lfz.com',`password`=sha1('LearningFuze')
+-- #4
+DELETE FROM `users`WHERE `username`= 'Pidgeon'
