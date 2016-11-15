ee7d1415b376d1f26b82ad76ee13597f92e1327a
John (Jef) Fisher <Fishe545@gmail.com>
2016-11-14 22:31:53 -0800

    not sure if I'm doing FS2 correctly or not

--- mysql_basics/mysql_queries.sql
+++ mysql_basics/mysql_queries.sql
@@ -1,5 +1,5 @@
 Place your queries below
--- //FS1
+-- //FS-1
 -- #1
 SELECT * FROM `users` WHERE `username` = 'Tandori'
 -- #2
@@ -8,3 +8,14 @@ UPDATE `users` SET `email` ='TayTay@lfz.com' WHERE `username` = 'Tyrannosaurus'
 INSERT INTO `users` SET `username`='Pidgeon', `email`='Sean@lfz.com',`password`=sha1('LearningFuze')
 -- #4
 DELETE FROM `users`WHERE `username`= 'Pidgeon'
+-- //FS-2
+
+-- #1
+
+-- #2
+
+-- #3
+
+-- #4
+
+-- #5
