1adcd76278c2d55d0dfda7f15c35c05b33d49d20
John (Jef) Fisher <Fishe545@gmail.com>
2016-11-15 11:15:34 -0800

    Finished but php storm is acting wonky

--- mysql_basics/mysql_queries.sql
+++ mysql_basics/mysql_queries.sql
@@ -9,13 +9,13 @@ INSERT INTO `users` SET `username`='Pidgeon', `email`='Sean@lfz.com',`password`=
 -- #4
 DELETE FROM `users`WHERE `username`= 'Pidgeon'
 -- //FS-2
-
 -- #1
-
+SELECT * FROM `todo_items` WHERE `user_id`=5
 -- #2
-
+INSERT INTO `todo_items`SET `title`='Hair', `details`='Just cut it', `timestamp`= UNIX_TIMESTAMP(), `user_id`=5
 -- #3
-
+DELETE FROM `todo_items` WHERE `id`= 12
 -- #4
-
+UPDATE `todo_items` SET `details`= 'Just fold it' WHERE `id`= 6
 -- #5
+SELECT * FROM `users`,`todo_items` WHERE `user_id`= 5 AND `users`.`id`= `todo_items`.`user_id`
